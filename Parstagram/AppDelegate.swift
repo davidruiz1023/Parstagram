46dd3e6ef9062d3dde05565f5e428b8813539600
David Ruiz <david.ruiz@churchbrothers.com>
2020-10-19 23:18:06 -0700

    working posts add and download

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -22,6 +22,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
             Parse.initialize(with: parseConfig)
         
+//        if PFUser.current() != nil {
+//            let main = UIStoryboard(name: "Main", bundle: nil)
+//            let feedNavigationController = main.instantiateViewController(identifier: "FeedNavigationController")
+//            
+//            window?.rootViewController = feedNavigationController
+//        }
+        
         return true
     }
 
---
+++ Parstagram/CommentTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CommentTableViewCell.swift
+//  Parstagram
+//
+//  Created by David Ruiz on 10/19/20.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
